0021b189f5504b1c3895cc0dcfe57bca97973f9a
Kimhanjin <ddang0103@naver.com>
2024-09-19 17:19:40 +0900

    Feature: Grapql 을 통한 유저 생성  로직작성
    Mutaition 및 GraphQl 사용

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,23 @@
-const { text } = require("express")
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+
 
 module.exports = {
+    createUser: async function({userInput}, req) {
+        // const email = args.userInput.email;
+        const exsistingUser = await User.findOne({email: userInput.email});
+        if(exsistingUser){
+            const error = new Error('User exists already');
+            throw error
+        }
+        const hashedPw = await bcrypt.hash(userInput.password, 12);
+        const user = new User({
+          email: userInput.email,
+          name: userInput.name,
+          password: hashedPw
+        });
+        const createdUser = await user.save();
+        return { ...createdUser._doc, _id: createdUser._id.toString() };
+    }
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,33 +1,41 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-    type Post{
-        _id: ID!,
-        title: String!,
-        content: String!,
-        imageUrl: String!,
-        creator: User!,
-        createdAt: String!,
+       type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
         updatedAt: String!
     }
-    type User{
-        _id: ID!,
-        email: String!,
-        name: String!,
-        password: String,
-        status: String!,
-        posts: [Post]!
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
     }
-    input userInputData {
-        email: String!,
-        name: String!,
+
+    input UserInputData {
+        email: String!
+        name: String!
         password: String!
     }
-    type RootMutation{
+
+    type RootQuery {
+        hello: String
+    }
+
+    type RootMutation {
         createUser(userInput: UserInputData): User!
     }
+
     schema {
-        mutation:
+        query: RootQuery
+        mutation: RootMutation
     }
- 
 `);
