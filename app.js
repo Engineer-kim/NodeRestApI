74a382a0bb8a6366d85133518fb1fedff92f5f1e
Kimhanjin <ddang0103@naver.com>
2024-09-19 22:46:19 +0900

    Fix: AuthJs에서의 인증처리가 아닌 리졸버에서 인증처리및 분기처리담당(리졸버로 위임, Auth => 미들웨어로 )

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const  {  graphqlHTTP } = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth')
 
 
 const cors = require('cors');
@@ -62,6 +63,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth)
 
 app.use('/graphql' , graphqlHTTP({
   schema: graphqlSchema,
